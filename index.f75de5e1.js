2bb6032ff558b52282383ad56a61fa4743adb564
MTS13 <mtstaras@gmail.com>
2024-06-17 23:29:04 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,17 @@
+"use strict";
+const population = document.querySelectorAll(".population");
+const arr = [
+    ...population
+].map((element)=>{
+    const value = element.textContent;
+    const number = value.replaceAll(",", "");
+    return Number(number);
+});
+const totalValue = arr.reduce((acc, elem)=>acc + elem, 0);
+const averageValue = totalValue / arr.length;
+const total = document.querySelector(".total-population");
+const average = document.querySelector(".average-population");
+total.innerText = totalValue.toLocaleString("en-US");
+average.innerText = averageValue.toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
